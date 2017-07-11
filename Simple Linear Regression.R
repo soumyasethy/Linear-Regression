b693d5cd85f30c0057c7a2011511d743d0027b34
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2017-07-11 15:11:59 +0530

    Updated self explonitary comment for better understanding the model.

--- Simple Linear Regression.R
+++ Simple Linear Regression.R
@@ -1,26 +1,38 @@
-#Simple linear regression model
+#############Simple linear regression model###########
+
+#Reading CSV File 
+library(readr)
+LR_car_sales <- read_csv("~/Desktop/Data Science/LR_car_sales.csv")
+#View imported Data
+View(LR_car_sales)
 data(cars)
+#Checking Number of Rows
 nrow(cars)
+#Checking Number of Columns
 ncol(cars)
+#Checking Column Names
 names(cars)
+#Checking Summary(Mean, Mode, Median)
 summary(cars)
-#to check missing values
+#Check missing values
 is.na(cars)
 is.na(cars$speed)
 is.na(cars$dist)
-#to check linear trend
+#Check linear trend by ploting graph
 plot(cars$speed)
 plot(cars$dist,col="red")
 plot(cars$dist,cars$speed,col=5)
-#to check outliers
+#Check outliers by ploting Box Plot Graph
 boxplot(cars$speed)
 boxplot(cars$dist)
 boxplot(cars,col=3)  
 
-#Linear regression model
+#Applying Linear Regression Model
 fit<-lm(speed~dist,cars)
 
+#Checking Summary after applying Linear Regression to the Model
 summary(fit)
 abline(fit)
+#Ploting Multiple graph
 par(mfrow=c(2,2))
 plot((fit))
