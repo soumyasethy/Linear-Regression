f485bcc4f8e5d7d5c81b6bd572be27cfab31812b
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2017-07-11 19:32:27 +0530

    Fixed Bugs & Comments

--- Simple Linear Regression.R
+++ Simple Linear Regression.R
@@ -1,32 +1,25 @@
-#############Applying Simple linear regression model###########
-rm(list = ls()) 
 #Reading CSV File 
 library(readr)
-cars <- read_csv("~/Desktop/Data Science/cars.csv")
+LR_car_sales <- read_csv("~/Desktop/Data Science/LR_car_sales.csv")
 #View imported Data
-#View(cars)
-#data(cars)
-head(cars)
-tail(cars)
+View(LR_car_sales)
+data(cars)
 #Checking Number of Rows
 nrow(cars)
 #Checking Number of Columns
 ncol(cars)
 #Checking Column Names
 names(cars)
-# Renaming the column name
-#Colnames(cars)[colnames(cars)== "speed"] <- "speed"
-#names(cars)
 #Checking Summary(Mean, Mode, Median)
 summary(cars)
 #Check missing values
-#is.na(cars)
-#is.na(cars$speed)
-#is.na(cars$dist)
-na.omit(cars)
+is.na(cars)
+is.na(cars$speed)
+is.na(cars$dist)
 #Check linear trend by ploting graph
 plot(cars$speed)
 plot(cars$dist,col="red")
+plot(cars$dist,cars$speed,col=5)
 #Check outliers by ploting Box Plot Graph
 boxplot(cars$speed)
 boxplot(cars$dist)
